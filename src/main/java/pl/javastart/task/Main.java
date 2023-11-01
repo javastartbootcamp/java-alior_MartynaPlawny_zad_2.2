9d18e793c702fa49617d559c7579e9cfa2ec4ccf
Martyna Plawny <kotnik909@gmail.com>
2023-11-01 21:47:58 +0100

    commit zadanie 2.2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,14 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        String nazwaMleko = "Mleko";
+        String nazwaCzekolada = "Czekolada";
+        String producentMleka = "Mlekowita";
+        String producentCzekolady = "Wedel";
+        String cenaMleka = "2.5zł";
+        String cenaCzekolady = "2.19zł";
 
+        System.out.printf("%s %s %s\n", nazwaMleko, producentMleka, cenaMleka);
+        System.out.printf("%s %s %s", nazwaCzekolada, producentCzekolady, cenaCzekolady);
     }
 }
